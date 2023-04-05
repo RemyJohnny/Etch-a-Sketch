ed64b04941b3910b5f39d26e935c9f9a78e5791c
RemyJohnny <ewerejohnnyjnr93@gmail.com>
2023-04-06 02:50:03 +0300

    project rebuild

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 let slider = document.getElementById('range');
 let value = document.querySelector('.value');
 const sketchpad = document.querySelector(".sketchpad");
-const resetBtn = document.querySelector('.reset-section');
+const resetBtn = document.querySelector('.reset');
 const RBGbtn = document.querySelector ('.RBG-button');
-const opaque = document.querySelector('.opaque-section');
-const blueBtn = document.querySelector('.blue');
-const blackBtn = document.querySelector('.black');
+const opaque = document.querySelector('.opaqueBtn');
+const Custom = document.querySelector('#customcolor');
+const clearAll = document.querySelector('.clearAll');
+const eraser = document.querySelector('.eraser');
 
 let cells ;
 let cellarr = [];
 let roundedValue;
+let mouse = 'false';
+let paint = 'custom';
   
 slider.value = value
 function calcValue () {
@@ -21,6 +24,8 @@ slider.addEventListener('input' , function(){
     value.textContent = `${this.value}%`;
        
 })
+
+
 //event to change cell size
 slider.addEventListener('change', function (){
     alert('choose color and draw');
@@ -29,6 +34,12 @@ slider.addEventListener('change', function (){
     pensize(roundedValue);
     cellarr =document.querySelectorAll('.cell');
 
+})
+document.body.addEventListener('pointerdown', function(){
+    mouse = 'true'
+})
+document.body.addEventListener('pointerup' , function(){
+    mouse = false;
 })
 // function to create cell append it to the parent box a grid
 function pensize (size) {
@@ -43,96 +54,152 @@ function pensize (size) {
     sketchpad.style.cssText = `display: grid; grid-template-columns:repeat(${size},1fr); grid-template-rows:repeat(${size},1fr);`;
 
 }
+function getColour(){
+    switch (true){
+        case (paint === 'custom'):
+            return `${Custom.value}`;
+        case (paint === 'random'):
+            return randomColor();
+        default:
+            return;    
 
-// event listner for reset button to reload the page 
-resetBtn.addEventListener('click', () => {
-    location.reload();
-})
-//event listner to RGB button to toggle on random color
-RBGbtn.addEventListener('click', () => {
-    
-    //loop to clear all backgroundcolor style when the RGB button is clicked
-    for (i = 0; i < cellarr.length; i++){
-        cellarr[i].style.backgroundColor = '';
-     }
-    
-         //event listner on forEach loop to activate pen with random colors
-        cellarr.forEach((cell) => {
-          cell.addEventListener('pointermove', function (e) {
-            // 'R' to get random number for red in Rgb()
-            //'B' to get random number for blue in rgb()
-            // 'G' to get random number for green in rgb()
+    }
+}
+function randomColor(){
          let R = Math.ceil((Math.random() * 255) +1)
          let B = Math.ceil((Math.random() * 255) +1)
          let G = Math.ceil((Math.random() * 255) +1)
          //'RBG' to concat R G B 
-            let RBG = `rgb(${R} ${G} ${B})`;
-         e.target.style.backgroundColor = RBG;
-        
-           })
+             return `rgb(${R} ${G} ${B})`;
+}
+
+// event listner for reset button to reload the page 
+resetBtn.addEventListener('click', () => {
+    resetBtn.classList.add('blink');
+    location.reload();
+})
+resetBtn.addEventListener('transitionend', () => {
+    resetBtn.classList.remove('blink');
+})
+clearAll.addEventListener('click', () =>{
+    clearAll.classList.add('blink');
+    for (i = 0; i < cellarr.length; i++){
+        cellarr[i].style.backgroundColor = '';
+    }
+})
+clearAll.addEventListener('transitionend', () =>{
+    clearAll.classList.remove('blink');
+})
+eraser.addEventListener('click', () => {
+    eraser.classList.add('blink'); 
+    cellarr.forEach((cell) => {
+        cell.addEventListener('mousedown', function (e) {
+           e.target.style.backgroundColor = '';
+                
         })
-      
+        cell.addEventListener('mouseenter', function (e) {
+            if (mouse === 'true'){
+            e.target.style.backgroundColor = '';
+            }
+            else{
+                return;
+            }
+        })
+    })  
+        
+})
+eraser.addEventListener('transitionend', () => {
+    eraser.classList.remove('blink');
+})
+//event listner to RGB button to toggle on random color
+RBGbtn.addEventListener('click', () => {
     
+    paint = 'random';
+        
+    cellarr.forEach((cell) => {
+        cell.addEventListener('mousedown', function (e) {
+           e.target.style.backgroundColor = getColour();
+                
+        })
+        cell.addEventListener('mouseenter', function (e) {
+            if (mouse === 'true'){
+            e.target.style.backgroundColor = getColour();
+            }
+            else{
+                return;
+            }
+        })
+    })  
         
 })
 //event listner to opaque button to toggle on opaque color
 opaque.addEventListener('click', () => {
-    
-    //loop to clear all backgroundcolor style when the opaque button is clicked
-    for (i = 0; i < cellarr.length; i++){
-        cellarr[i].style.backgroundColor = '';
-    }
-    
+    opaque.classList.add('blink');
     cellarr.forEach((cell) => {
-        // 'i' to increase the opacity on every move of the pointer
-        let i =0.09
-        cell.addEventListener('pointermove', (e) => {
-            
-            i += 0.09
-            e.target.style.backgroundColor = `rgb(0,0,0,${i})`;
+        let i = 0.1;
+        cell.addEventListener('mousedown', function (e) {
+            i +=0.1;
+            e.target.style.backgroundColor = `rgba(0,0,0,${i})`;
+                
         })
-
+        cell.addEventListener('mouseenter', function (e) {
+         i  += 0.1;   
+            if (mouse === 'true'){
+                e.target.style.backgroundColor = `rgba(0,0,0,${i})`;
+            }
+            else{
+                return;
+            }
+        })
     })
     
- })
+})
+opaque.addEventListener('transitionend', () => {
+    opaque.classList.remove('blink');
+})
  
- //event listner to black button to toggle on black color
- blackBtn.addEventListener('click', () => {
-    
-    //loop to clear all backgroundcolor style when the black button is clicked
-    for (i = 0; i < cellarr.length; i++){
-        cellarr[i].style.backgroundColor = '';
-     }
-         // forEach loop to add black backgroundcolor on pointermove
+    Custom.addEventListener('change', () => {
+
+        paint = 'custom';
+        
         cellarr.forEach((cell) => {
-          cell.addEventListener('pointermove', function (e) {
-         
-         e.target.style.backgroundColor = "black";
-        
-           })
+            cell.addEventListener('mousedown', function (e) {
+               e.target.style.backgroundColor = getColour();
+                    
+            })
+            cell.addEventListener('mouseenter', function (e) {
+                if (mouse === 'true'){
+                e.target.style.backgroundColor = getColour();
+                }
+                else{
+                    return;
+                }
+            })
         })
-    
-    
+    })  
+    Custom.addEventListener('click', () => {
+
+        paint = 'custom';
         
-})
-//event listner to blue button to toggle on blue color
-blueBtn.addEventListener('click', () => {
-    
-    //loop to clear all backgroundcolor style when the blueBtn button is clicked
-    for (i = 0; i < cellarr.length; i++){
-        cellarr[i].style.backgroundColor = '';
-     }
-         //forEach loop to add blue backgroundColor on mousemove
         cellarr.forEach((cell) => {
-          cell.addEventListener('pointermove', function (e) {
-         
-         e.target.style.backgroundColor = "blue";
-        
-           })
+            cell.addEventListener('mousedown', function (e) {
+               e.target.style.backgroundColor = getColour();
+                    
+            })
+            cell.addEventListener('mouseenter', function (e) {
+                if (mouse === 'true'){
+                e.target.style.backgroundColor = getColour();
+                }
+                else{
+                    return;
+                }
+            })
         })
-    
+    })  
         
-})
+            
+
+   
 
 
 
